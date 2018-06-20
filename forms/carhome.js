70af6b44e5dca2fb7d50d0ef7e82719e52253772
matthiasliszt <mliszt.program@gmail.com>
2018-06-20 14:03:40 +0200

    new forms

--- forms/carhome.js
+++ forms/carhome.js
@@ -3,13 +3,30 @@ var carData;
 
 
 function showCars(){
+ var cars=[];
+ carData.pop();
+ carData.map(function(x){var e=`<div class="col-sm-5 col-md-5 col-lg-5 car">
+ 	                            <img class="dummyimage" src="../pics/${x.img}" />
+ 	                            <p>${x.carType}</p>
+ 	                            <p>${x.power} kW</p>
+ 	                            <p>${x.prodYear}</p>   
+ 	                            </div>
+ 	                           `;
+ 	                     cars.push(e);      
+
+                        });	
+ //console.log(cars.join(''));
+ $("#showCars").html(cars.join(''));	
 	
 }
 
 function parseCarData(){	
  var data=document.getElementById('dbData').textContent;
  var p=JSON.parse(data);
- console.log(JSON.stringify(p));
+ //console.log(JSON.stringify(p));
+ carData=p;
 }
 
-setInterval(parseCarData,3000);
+setTimeout(parseCarData,1500);
+setTimeout(showCars,1500);
+
